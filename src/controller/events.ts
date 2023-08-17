9cae037ff75bcee3ec7b89cb8c256e6fe2665dc3
Maryinfun <motmaryion@gmail.com>
2023-08-17 19:29:58 +0300

    feat: add check login forma

--- src/controller/events.ts
+++ src/controller/events.ts
@@ -1,15 +1,40 @@
 import Login from "./login";
+import Checks from "./checks";
 
 class Events {
   login: Login;
+  checks: Checks;
   constructor() {
     this.login = new Login();
+    this.checks = new Checks();
   }
   clickButtonLogin(loginBtn: HTMLButtonElement) {
     loginBtn.addEventListener("click", () => {
-      this.login.login();
-      console.log(loginBtn);
+      if (Checks.emailErrorStatus && Checks.passwordErrorStatus) {
+        this.login.login();
+      }
     });
+  }
+
+  inputFilling(
+    input: HTMLInputElement,
+    area: HTMLElement,
+    checkMethod: "email" | "password",
+  ) {
+    switch (checkMethod) {
+      case "email":
+        input.addEventListener("input", () => {
+          this.checks.printErrors(area, this.checks.checkEmail(input.value));
+        });
+        break;
+      case "password":
+        input.addEventListener("input", () => {
+          this.checks.printErrors(area, this.checks.checkPassword(input.value));
+        });
+        break;
+      default:
+        break;
+    }
   }
 }
 
--- src/pages/login_page/login_page.ts
+++ src/pages/login_page/login_page.ts
@@ -1,10 +1,13 @@
 import "./login_page.scss";
 import Events from "../../controller/events";
+import Checks from "../../controller/checks";
 
 class LoginPage {
   events: Events;
+  checks: Checks;
   constructor() {
     this.events = new Events();
+    this.checks = new Checks();
   }
 
   render(): HTMLElement {
@@ -17,19 +20,60 @@ class LoginPage {
 
     const inputLogEmail: HTMLInputElement = document.createElement("input");
     inputLogEmail.classList.add("input-log-email");
+    inputLogEmail.placeholder = "Enter email";
+
+    const emailErrorArea: HTMLElement = document.createElement("span");
+    emailErrorArea.classList.add("main__email-error");
 
     const inputLogPassword: HTMLInputElement = document.createElement("input");
+    const inputLogOpenView: HTMLElement = document.createElement("span");
+    const passwordWrapper: HTMLElement = document.createElement("div");
     inputLogPassword.classList.add("input-log-password");
+    inputLogOpenView.classList.add("main__password-open-view");
+    passwordWrapper.classList.add("main__password-wrapper");
+    passwordWrapper.append(inputLogPassword, inputLogOpenView);
+    inputLogPassword.type = "password";
+    inputLogPassword.placeholder = "Enter password";
+
+    const passwordErrorArea: HTMLElement = document.createElement("span");
+    passwordErrorArea.classList.add("main__password-error");
 
     const btnLog: HTMLButtonElement = document.createElement("button");
     btnLog.classList.add("btn-login");
     btnLog.textContent = "Login";
 
     mainWrapper.innerHTML = "";
-    mainWrapper.append(caption, inputLogEmail, inputLogPassword, btnLog);
+    mainWrapper.append(
+      caption,
+      inputLogEmail,
+      emailErrorArea,
+      passwordWrapper,
+      passwordErrorArea,
+      btnLog,
+    );
 
+    this.showHidePassword(inputLogOpenView, inputLogPassword);
     this.events.clickButtonLogin(btnLog);
+    this.events.inputFilling(inputLogEmail, emailErrorArea, "email");
+    this.events.inputFilling(inputLogPassword, passwordErrorArea, "password");
+
     return mainWrapper;
+  }
+
+  showHidePassword(
+    inputLogOpenView: HTMLElement,
+    inputLogPassword: HTMLInputElement,
+  ) {
+    inputLogOpenView.addEventListener("click", () => {
+      inputLogOpenView.classList.toggle("main__password-open-view-show");
+      if (
+        inputLogOpenView.classList.contains("main__password-open-view-show")
+      ) {
+        inputLogPassword.type = "text";
+      } else {
+        inputLogPassword.type = "password";
+      }
+    });
   }
 }
 
