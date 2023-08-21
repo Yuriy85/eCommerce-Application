84fddd920e9f72885cba565c4a48738bed5f172d
Maryinfun <motmaryion@gmail.com>
2023-08-21 14:41:46 +0300

    feat: add titles to links

--- src/pages/main_page/main_page.ts
+++ src/pages/main_page/main_page.ts
@@ -16,31 +16,31 @@ class MainPage {
 
     const linkToCatalog: HTMLLIElement = document.createElement("li");
     linkToCatalog.classList.add("main-page__link");
-    linkToCatalog.innerHTML = `<a href = "${pagePaths.catalogPath}">Go to catalog</a>`;
+    linkToCatalog.innerHTML = `<a href = "${pagePaths.catalogPath}" title="Go to catalog">Catalog</a>`;
 
     const linkToDetail: HTMLLIElement = document.createElement("li");
     linkToDetail.classList.add("main-page__link");
-    linkToDetail.innerHTML = `<a href = "${pagePaths.detailedPath}">Go to detailed</a>`;
+    linkToDetail.innerHTML = `<a href = "${pagePaths.detailedPath}" title="Go to detailed">Detailed</a>`;
 
     const linkToProfile: HTMLLIElement = document.createElement("li");
     linkToProfile.classList.add("main-page__link");
-    linkToProfile.innerHTML = `<a href = "${pagePaths.profilePath}">Go to profile</a>`;
+    linkToProfile.innerHTML = `<a href = "${pagePaths.profilePath}" title="Go to profile">Profile</a>`;
 
     const linkToLogin: HTMLLIElement = document.createElement("li");
     linkToLogin.classList.add("main-page__link");
-    linkToLogin.innerHTML = `<a href = "${pagePaths.loginPath}">Go to login</a>`;
+    linkToLogin.innerHTML = `<a href = "${pagePaths.loginPath}" title="Go to login">Login</a>`;
 
     const linkToRegister: HTMLLIElement = document.createElement("li");
     linkToRegister.classList.add("main-page__link");
-    linkToRegister.innerHTML = `<a href = "${pagePaths.registerPath}">Go to register</a>`;
+    linkToRegister.innerHTML = `<a href = "${pagePaths.registerPath}" title="Go to register">Register</a>`;
 
     const linkToBasket: HTMLLIElement = document.createElement("li");
     linkToBasket.classList.add("main-page__link");
-    linkToBasket.innerHTML = `<a href = "${pagePaths.basketPath}">Go to basket</a>`;
+    linkToBasket.innerHTML = `<a href = "${pagePaths.basketPath}" title="Go to basket">Basket</a>`;
 
     const linkToAbout: HTMLLIElement = document.createElement("li");
     linkToAbout.classList.add("main-page__link");
-    linkToAbout.innerHTML = `<a href = "${pagePaths.aboutPath}">Go to about us</a>`;
+    linkToAbout.innerHTML = `<a href = "${pagePaths.aboutPath}" title="Go to about us">About Us</a>`;
 
     linksToPages.append(
       linkToCatalog,
